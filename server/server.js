2a316e8eed886e4981afa9abb7ea41353b823ec1
leeric92 <hle2806@uw.edu>
2015-08-11 15:56:06 -0700

    adding socket.io functionality

--- server/server.js
+++ server/server.js
@@ -31,6 +31,7 @@ require('./config/middleware.js')(app, express);
 // export our app for testing and flexibility, required by index.js
 
 server.listen(process.env.PORT || 8000);
+
 // Declare io for the socket... Just creating an instance of the sokcet library
 var io = require('socket.io')(server);
 
