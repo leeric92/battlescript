85857b2fec0dee38882107b8e9ff78f02beeaaf7
leeric92 <hle2806@uw.edu>
2015-08-14 15:56:27 -0700

    pushing heroku

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -133,7 +133,7 @@ angular.module('battlescript.services', [])
   var createSocket = function(route, params) {
     console.log('attempt to create');
     var query = params.join('&');
-    return io.connect('https://vast-wildwood-2487.com/#/' + route, {
+    return io.connect('vast-wildwood-2487.herokuapp.com/#/' + route, {
       query: query,
       'force new connection': true
     });
